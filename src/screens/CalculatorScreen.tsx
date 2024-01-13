0a33674c73f87e51f7dbd2dc116b0b0a13e2508b
Facundo Guardia <101948163+facuguardia@users.noreply.github.com>
2024-01-13 11:17:31 -0300

    Update eslint rules and add CalculatorScreen component

---
+++ src/screens/CalculatorScreen.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+export default function CalculatorScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Hola Facu</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
